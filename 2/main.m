6e22c5a3879823db26854a583df9f510aad12bdb
Harrison Chen <99154196+ch77ancreant@users.noreply.github.com>
2022-08-18 17:03:22 +0800

    Update main.m

--- 2/main.m
+++ 2/main.m
@@ -1,8 +1,8 @@
-clear; clc;
 
 % Both x1 and x2 have 8 points
 x1 = 1:8;
-x2 = [zeros(1,4),ones(1,4)];
+x2 = [zeros(1,4), ones(1,4)];
 
 % Only need 8 points DFT
-[X1, X2] = dftreal(x1,x2)
+[X1, X2] = dftreal(x1, x2)
+
